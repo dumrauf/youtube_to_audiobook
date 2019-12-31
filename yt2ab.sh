a0e90931f69d0fce00b7396e90ed45589ba8ab73
Dominic Dumrauf (MacBook Pro 2017) <dominic-git-macbook-pro-2017@dumrauf.de>
2019-12-31 16:40:04 +0000

    Adding native playlist handling
    This closes #1

--- yt2ab.sh
+++ yt2ab.sh
@@ -59,6 +59,31 @@ function echo_debug {
 }
 
 
+# Helper function for downloading and converting YouTube videos from a given URL
+function download_and_convert_youtube_video () {
+	_url=$1
+	# Dynamic file name definitions
+	_youtube_filename=$(youtube-dl --get-filename "${_url}")
+	_cover="${_youtube_filename}".jpg
+	_youtube_audio="${_youtube_filename}"-youtube-audio.mp3
+	_audiobook_filename="${_youtube_filename}.mp3"
+
+	# Ensure proper cleanup upon function return
+	trap 'rm -f "${_cover}"; rm -f "${_youtube_audio}"' RETURN
+
+	# Download thumbnail and audio from given YouTube ${_url}
+	youtube-dl "${_url}" --write-thumbnail --skip-download -o "${_cover}"
+	youtube-dl "${_url}" -x --audio-format mp3 --audio-quality 0 -o "${_youtube_audio}"
+
+	# Convert audio to Mp3, including thumbnail for easier recognition
+	ffmpeg -i "${_youtube_audio}" -i "${_cover}" -map_metadata 0 -map 0 -map 1 -codec:a libmp3lame -qscale:a ${audio_quality} -filter:a "atempo=${audio_speed}" "${_audiobook_filename}"
+
+	# Remove original extension from ${_audiobook_filename}
+	_base=${_audiobook_filename%.*.mp3}
+	mv "${_audiobook_filename}" "${_base}.mp3" 
+}
+
+
 
 
 ###############################################################################
@@ -66,7 +91,7 @@ function echo_debug {
 ###############################################################################
 
 # Check dependencies of Bash script
-DEPENDENCIES='youtube-dl ffmpeg'
+DEPENDENCIES='youtube-dl ffmpeg jq'
 check_dependencies "${DEPENDENCIES}"
 
 
@@ -113,31 +138,23 @@ echo_debug "Audio quality set to ${audio_quality}"
 #                           YouTube Audio Download                            #
 ###############################################################################
 
+# Retrieve the flat JSON representation of the "${url}" playlist (using the fact
+# that 'youtube-dl' produces the identical JSON format for both single videos 
+# as well as playlists) and store each result in an array in order to avoid
+# interferences with later outputs by other programmes
+declare -a video_jsons
+while IFS= read -r video_json
+do
+	video_jsons+=("${video_json}")
+done < <(youtube-dl -j --flat-playlist "${url}")
 
-# Helper function for cleaning up temporary files
-function finish {
-	rm "${cover}"
-	rm "${youtube_audio}"
-}
-
-# Dynamic file name definitions
-youtube_filename=$(youtube-dl --get-filename "${url}")
-cover="${youtube_filename}".jpg
-youtube_audio="${youtube_filename}"-youtube-audio.mp3
-audiobook_filename="${youtube_filename}.mp3"
-
-# Ensure proper cleanup, regardless of the exit status of the script
-trap finish EXIT
-
-
-# Download thumbnail and audio from given YouTube ${url}
-youtube-dl "${url}" --write-thumbnail --skip-download -o "${cover}"
-youtube-dl "${url}" -x --audio-format mp3 --audio-quality 0 -o "${youtube_audio}"
-
-# Convert audio to Mp3, including thumbnail for easier recognition
-ffmpeg -i "${youtube_audio}" -i "${cover}" -map_metadata 0 -map 0 -map 1 -codec:a libmp3lame -qscale:a ${audio_quality} -filter:a "atempo=${audio_speed}" "${audiobook_filename}"
-
-
-# Remove original extension from ${audiobook_filename}
-base=${audiobook_filename%.*.mp3}
-mv "${audiobook_filename}" "${base}.mp3" 
+# Download and convert each video in "${video_jsons}"; inspired by <https://superuser.com/a/1359132>
+for video_json in "${video_jsons[@]}"
+do
+	title=$(jq -r '.title' <<< "${video_json}")
+	id=$(jq -r '.id' <<< "${video_json}")
+	video_url="https://youtu.be/${id}"
+	echo "Processing ${video_url}: '${title}'..."
+	download_and_convert_youtube_video "${video_url}"
+	echo "...done processing '${title}'!"
+done
